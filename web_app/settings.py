f2f79175bc44fc7c2fe742cb92c213b4b62284d7
smguri002 <57817751+smguri002@users.noreply.github.com>
2026-07-13 20:01:29 -0400

    addin efficient code to view

--- web_app/settings.py
+++ web_app/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
