44f80a52f36c5e681039943df04a1186a44bcfba
smguri002 <57817751+smguri002@users.noreply.github.com>
2026-07-16 14:48:25 -0400

    added powerhsell script

---
+++ find_clusters.ps1
@@ -0,0 +1,10 @@
+﻿$region = "us-east-1"
+
+$clusters = aws eks list-clusters --region $region --query 'clusters[]' --output text
+$clusters = $clusters -split "\s+"
+
+foreach ($c in $clusters) {
+    Write-Host "=== $c ===" -ForegroundColor Cyan
+    aws eks update-kubeconfig --name $c --region $region | Out-Null
+    kubectl get deployments -A
+}
